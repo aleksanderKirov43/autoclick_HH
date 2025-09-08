571ef4b6513aaeb29ca8673f484d38bc39b49b3e
aleksanderKirov43 <region-manager43@yandex.ru>
2025-09-08 09:18:49 +0300

    update filter

--- internal/service/auto.go
+++ internal/service/auto.go
@@ -29,7 +29,7 @@ func (s *AutoService) AutoRespond(token, resumeID string, vacancies []hhclient.V
 		"бровист", "визажист"}
 
 	// Список компаний, на которые не нужно откликаться
-	excludedCompanies := []string{"ozon", "wildberries", "т-банк", "мтс", "магнит", "суточно.ру", "VK", "Вконтакте"}
+	excludedCompanies := []string{"ozon", "wildberries", "т-банк", "мтс", "магнит", "суточно.ру", "VK", "Вконтакте", "МойОфис", "Солюшен"}
 
 	for _, v := range vacancies {
 		nameLower := strings.ToLower(v.Name)
